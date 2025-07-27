0cbe2a7e9ec358298f220c72c558356894f1ce69
Otisb759 <otisb230@gmail.com>
2025-07-26 21:39:20 -0400

    Update navigation.tsx

--- client/src/components/navigation.tsx
+++ client/src/components/navigation.tsx
@@ -95,7 +95,8 @@ export default function Navigation() {
                   </Button>
                 </Link>
                 <div className="pt-4 border-t">
-                  <p className="text-sm text-gray-600">info@ieo-us.org</p>
+                  <p className="text-sm text-gray-600">info@internationaleconomic.org
+</p>
                   <p className="text-sm font-semibold text-gray-900">(651) 300-9503</p>
                 </div>
               </nav>
